6cbe267edd712028ac4d51a5721328cdd9bdb474
Rustem Kakimov <ruskakimov@gmail.com>
2024-07-23 13:32:37 -0400

    Implement logic

--- app/src/main/java/com/example/kpop/idol/lecture/lecture/midterm1/MainActivity.kt
+++ app/src/main/java/com/example/kpop/idol/lecture/lecture/midterm1/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.kpop.idol.lecture.lecture.midterm1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +22,43 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val operationSpinner: Spinner = findViewById(R.id.operation_spinner)
+        val number1EditText: EditText = findViewById(R.id.number_1)
+        val number2EditText: EditText = findViewById(R.id.number_2)
+        val calculateButton: Button = findViewById(R.id.calculate_btn)
+        val resultTextView: TextView = findViewById(R.id.result_txt)
+
+        val operationOptions = arrayOf("Add", "Subtract", "Multiply", "Divide")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, operationOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        operationSpinner.adapter = adapter
+
+        var selectedOperation = operationOptions.get(0)
+
+        operationSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                selectedOperation = operationOptions.get(p2)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        calculateButton.setOnClickListener {
+            val number1 = number1EditText.text.toString().toDouble()
+            val number2 = number2EditText.text.toString().toDouble()
+
+            val result = when (selectedOperation) {
+                "Add" -> number1 + number2
+                "Subtract" -> number1 - number2
+                "Multiply" -> number1 * number2
+                "Divide" -> number1 / number2
+                else -> 0.0
+            }
+            resultTextView.text = result.toString()
         }
     }
 }
